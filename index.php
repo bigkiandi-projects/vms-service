a3ff56ef3e760dd857b153cdb3e3fc478dbf7445
Bigkiandi <bigkianditeam@gmail.com>
2024-01-17 21:56:20 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@900&display=swap" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    <title>VMS Viewer V1</title>
+
+    <style type="text/css">
+      body {
+        font-family: 'Noto Sans KR', sans-serif;
+      }
+      .wind-speed {
+        background-color: #000;
+      }
+      .wind-speed h1, span, i {
+        color: yellow;
+        font-size: 150px;
+      }
+    </style>
+  </head>
+  <body style="background-color: #000 ;">
+    
+    <div class="text-center">
+
+          <div id="carouselExampleInterval" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+
+              <div class="carousel-item active" data-bs-interval="3500" style="width:100%">
+                <img src="assets/upload/l1.jpeg" class="d-block w-100" alt="..." height="840">
+              </div>
+
+              <div class="carousel-item" data-bs-interval="3500" style="width:100%">
+                <img src="assets/upload/l2.jpeg" class="d-block w-100" alt="..." height="840">
+              </div>
+
+              <div class="carousel-item non-img" data-bs-interval="3500" style="width:100%">
+                <div class="wind-speed" style="height: 840px; border: solid yellow 8px;">
+                  <h1>Kecepatan Angin</h1>
+                  <h1 class="wind-speed"><span class="speed"></span>
+                    <i class="fa-solid fa-wind"></i>
+                  </h1>
+                  <h1>Km / Jam</h1>
+                </div>
+              </div>
+
+            </div>
+          </div>
+      
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+
+      var appid = 'cbe5251478dbaad525f838e3007a9f8d';
+      var city = 'Poka';
+
+      $(document).ready(function() {
+
+        function getDataWind() {
+          $.getJSON("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+appid+"", function(result){
+              var kph = result.wind.speed*3600/1000;
+              var final = kph.toFixed(1);
+            $('.speed').text(final);
+          });
+        }
+
+        getDataWind();
+
+        setInterval(function(){
+          getDataWind()
+        }, 500000);
+
+      });
+    </script>
+    
+  </body>
+</html>
